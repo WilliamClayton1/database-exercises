4c2687b52f5550f24e604d3345cbecd9b3f44907
William Clayton <william.d.clayton1@gmail.com>
2023-10-23 17:08:46 -0500

    -wc

--- delete_migration.sql
+++ delete_migration.sql
@@ -1,10 +1,10 @@
 use fenrir;
 
-select artist from albums where release_date > 1991;
+select album from albums where release_date > 1991;
 delete from albums where release_date > 1991;
 
-select artist from albums where genre = 'Disco';
+select album from albums where genre = 'Disco';
 delete from albums where genre = 'Disco';
 
-select artist from albums where artist = 'Whitney Houston';
+select album from albums where artist = 'Whitney Houston';
 delete from albums where artist = 'Whitney Houston';
