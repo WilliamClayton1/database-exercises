082a49e4acc4553f75c4a16830c488bce450cc9d
William Clayton <william.d.clayton1@gmail.com>
2023-10-24 14:59:54 -0500

    -wc

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,7 +2,7 @@ USE employees;
 
 -- SELECT * FROM employees WHERE first_name in ('Irene', 'Vidya', 'Maya');
 
-SELECT * FROM employees WHERE gender = 'M' AND first_name = 'Irene' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT * FROM employees WHERE gender = 'M' AND first_name = 'Irene' OR gender = 'M' AND first_name = 'Vidya' OR gender = 'M' AND first_name = 'Maya';
 
 -- SELECT * FROM employees WHERE last_name like 'E%';
 
