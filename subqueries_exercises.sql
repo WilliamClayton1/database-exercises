37e9c9f212cdb0b50ce35b0bc8203ddcf9b3eed1
William Clayton <william.d.clayton1@gmail.com>
2023-10-30 09:49:36 -0500

    -wc

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,9 +1,28 @@
 USE employees;
 
-SELECT first_name, last_name, birth_date
+-- Selecting all employees with the same hire date
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+    );
+
+-- Selecting from titles where the first name of the employee is equal to "Aamod"
+SELECT *
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
+
+-- Selecting from department managers who are female
+SELECT first_name, last_name, gender
 FROM employees
 WHERE emp_no IN (
     SELECT emp_no
     FROM dept_manager
-)
-LIMIT 10;
+    WHERE gender = 'F' AND to_date LIKE '9999%'
+    );
